bcfd3ce5d812b0fa59601c471be80af284685e50
anizmo <potdar.anuj@gmail.com>
2022-11-19 14:29:53 -0500

    Handling for heroku failure

--- app.js
+++ app.js
@@ -16,4 +16,6 @@ TuitsController(app);
 HelloController(app);
 UserController(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
